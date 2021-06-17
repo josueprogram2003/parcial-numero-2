60f069e90454c7ece93473a1357f6e2364c65948
Jdominguez <josuedominguezonline12@gmail.com>
2021-06-17 16:02:24 -0500

    Correcion

--- js/form.js
+++ js/form.js
@@ -1,4 +1,5 @@
-
+$(document).ready(function () {
+});
 
 $("#boton").on("click", function(e){
   e.preventDefault();
@@ -8,19 +9,18 @@ alert("click")
   let fila1 = $("<li>");
   let fila2 = $("<li>");
   let fila3 = $("<li>");
-  let fila4 = $("<li>");
+
 
   fila1.val()=$("#nombre").val();
   console.log(fila1);
 
-  fila2.val()=$("#")
+  fila2.val()=$("#correo").val();
 
+  fila3.val()=$("#telefono").val();
 
   lista.append(lista1);
   lista.append(lista2);
   lista.append(lista3);
-  lista.append(lista4);
 
-  
-
-})
+  respuesta.append(lista);
+});
