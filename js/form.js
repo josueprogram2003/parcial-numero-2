00ad4b23c3a12bab5877c26407bd02b552b8b7f5
Jdominguez <josuedominguezonline12@gmail.com>
2021-06-17 16:17:54 -0500

    Ultima modificacion ya no me queda tiempo

--- js/form.js
+++ js/form.js
@@ -1,26 +1,23 @@
-$(document).ready(function () {
-});
+$(document).ready(function () {});
 
-$("#boton").on("click", function(e){
+const nombre = $("#nombre");
+const correo = $("#correo");
+const telefono = $("#telefono");
+const respuesta = $("#respuesta");
+
+$("#boton").on("click", function (e) {
   e.preventDefault();
-alert("click")
-
-  let lista = $("<ul>");
-  let fila1 = $("<li>");
-  let fila2 = $("<li>");
-  let fila3 = $("<li>");
-
-
-  fila1.val()=$("#nombre").val();
-  console.log(fila1);
-
-  fila2.val()=$("#correo").val();
-
-  fila3.val()=$("#telefono").val();
-
-  lista.append(lista1);
-  lista.append(lista2);
-  lista.append(lista3);
-
-  respuesta.append(lista);
+  const ul = $("<ul>");
+  let agrega =
+    "<li>" +
+    nombre.val() +
+    "</li>" +
+    "<li>" +
+    correo.val() +
+    "</li>" +
+    "<li>" +
+    telefono.val() +
+    "</li>";
+  ul.append(agrega);
+  respuesta.append(ul);
 });
